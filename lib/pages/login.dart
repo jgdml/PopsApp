0f011771596e908967a8d2d229a845112294a749
jgdml <jgdml2801@gmail.com>
2022-02-12 11:06:45 -0300

    tamanho do container

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,166 +4,171 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 60, left: 40, right: 40),
-        color: Color.fromARGB(255, 34, 34, 34),
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: 128,
-              height: 128,
-              child: Image.asset("assets/popsicle.png"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              // autofocus: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
+      backgroundColor: Color.fromARGB(255, 34, 34, 34),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Container(
+            padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  width: 128,
+                  height: 128,
+                  child: Image.asset("assets/popsicle.png"),
                 ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              // autofocus: true,
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            Container(
-              height: 40,
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                child: Text(
-                  "Recuperar Senha",
-                  textAlign: TextAlign.right,
-                ),
-                onPressed: () {
-                  
-                },
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1],
-                  colors: [
-                    Color.fromARGB(255, 216, 20, 20),
-                    Color(0XFFF92B7F),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("assets/popsicle.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
+                TextFormField(
+                  // autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
                   ),
-                  onPressed: () {
-                    
-                  },
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 211, 19, 108),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Cadastrar",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("assets/popsicle.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
+                TextFormField(
+                  // autofocus: true,
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
                   ),
-                  onPressed: () {},
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 40,
-              child: FlatButton(
-                child: Text(
-                  "Cadastre-se",
-                  textAlign: TextAlign.center,
+                Container(
+                  height: 40,
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    child: Text(
+                      "Recuperar Senha",
+                      textAlign: TextAlign.right,
+                    ),
+                    onPressed: () {
+                      
+                    },
+                  ),
                 ),
-                onPressed: () {
-                  
-                },
-              ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [0.3, 1],
+                      colors: [
+                        Color.fromARGB(255, 216, 20, 20),
+                        Color(0XFFF92B7F),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: SizedBox.expand(
+                    child: FlatButton(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Login",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Container(
+                            child: SizedBox(
+                              child: Image.asset("assets/popsicle.png"),
+                              height: 28,
+                              width: 28,
+                            ),
+                          )
+                        ],
+                      ),
+                      onPressed: () {
+                        
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 211, 19, 108),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: SizedBox.expand(
+                    child: FlatButton(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Cadastrar",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Container(
+                            child: SizedBox(
+                              child: Image.asset("assets/popsicle.png"),
+                              height: 28,
+                              width: 28,
+                            ),
+                          )
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 40,
+                  child: FlatButton(
+                    child: Text(
+                      "Cadastre-se",
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: () {
+                      
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
