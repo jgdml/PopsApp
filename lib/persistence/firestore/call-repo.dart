56f8a45fb9f237a3f67cb3abad9b67bcf69692ea
Kairo <kairo.controlenamao@gmail.com>
2022-02-14 20:33:50 -0300

    Constantes com o nome do repositório no Firestore

--- lib/persistence/firestore/call-repo.dart
+++ lib/persistence/firestore/call-repo.dart
@@ -4,43 +4,39 @@ import 'package:pops_app/core/model/call.dart';
 import 'package:pops_app/core/model/status-enum.dart';
 import 'package:pops_app/core/model/user.dart';
 
-class CallRepo{
+class CallRepo {
+  late CollectionReference callCollection;
 
-    late CollectionReference callCollection;
+  static const String REPO_NAME = 'call';
 
-    CallRepo(){
-        callCollection = FirebaseFirestore.instance.collection('call');
+  CallRepo() {
+    callCollection = FirebaseFirestore.instance.collection(REPO_NAME);
+  }
+
+  Future<List<Call>> find() async {
+    var res = await callCollection.get();
+
+    var lista = res.docs.map((doc) => Call(
+          id: doc.reference.id.toString(),
+          active: doc[Call.ACTIVE],
+          receiver: User.fromJson(doc[Call.RECEIVER]),
+          caller: User.fromJson(doc[Call.CALLER]),
+          startTime: DateTime.parse(doc[Call.START_TIME]),
+          endTime: DateTime.parse(doc[Call.END_TIME]),
+          status: StatusEnumExtension.fromRaw(doc[Call.STATUS]),
+        ));
+    return lista.toList();
+  }
+
+  delete(id) async {
+    await callCollection.doc(id).set({Call.ACTIVE: false});
+  }
+
+  saveOrUpdate(Call call) async {
+    if (call.id.toString().isEmpty || call.id.toString() == 'null') {
+      await callCollection.doc().set(call.toJson());
+    } else {
+      await callCollection.doc(call.id.toString()).set(call.toJson());
     }
-
-    Future<List<Call>> find() async {
-        var res = await callCollection.get();
-
-        var lista = res.docs.map(
-            (doc) => Call(
-              id: doc.reference.id.toString(),
-              active: doc[Call.ACTIVE],
-              receiver: User.fromJson(doc[Call.RECEIVER]),
-              caller: User.fromJson(doc[Call.CALLER]),
-              startTime: DateTime.parse(doc[Call.START_TIME]),
-              endTime: DateTime.parse(doc[Call.END_TIME]),
-              status: StatusEnumExtension.fromRaw(doc[Call.STATUS]),
-            )
-        );
-        return lista.toList();
-    }
-
-    delete(id) async {
-        await callCollection.doc(id).set({
-            Call.ACTIVE: false
-        });
-    }
-
-    saveOrUpdate(Call call) async {
-      if (call.id.toString().isEmpty || call.id.toString() == 'null'){
-        await callCollection.doc().set(call.toJson());
-      }
-      else{
-        await callCollection.doc(call.id.toString()).set(call.toJson());
-      }
-    }
+  }
 }
