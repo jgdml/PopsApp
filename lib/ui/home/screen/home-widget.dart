fe52812fd4c45d8e4f758a24ef36b6831c21485e
jgdml <jgdml2801@gmail.com>
2022-02-16 11:58:36 -0300

    Drawer mostrando info / botao sair

--- lib/ui/home/screen/home-widget.dart
+++ lib/ui/home/screen/home-widget.dart
@@ -18,207 +18,261 @@ import '../../utils/util.dart';
 import 'home-page.dart';
 
 class HomeWidget extends State<HomeScreen> {
-    final HomeController _controller = HomeController();
-    final MapController mapController = MapController();
-    final Util util = Util();
+  final HomeController _controller = HomeController();
+  final MapController mapController = MapController();
+  final Util util = Util();
 
-    var calls = [];
-    var icemen = <User>[];
-    LatLng? userLocation;
+  var calls = [];
+  var icemen = <User>[];
+  LatLng? userLocation;
 
-    @override
-    void initState() {
-        super.initState();
-        _getUserLocation();
-        _getUser();
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+    _getUser();
+  }
+
+  _getUser() async {
+    await _controller.checkUser();
+    if (_controller.getUserRole() != null &&
+        _controller.getUserRole() == RoleEnum.ROLE_ICEMAN) {
+      FirebaseFirestore.instance
+          .collection(CallRepo.REPO_NAME)
+          .snapshots()
+          .listen((event) {
+        calls = event.docs;
+      });
     }
+  }
 
-    _getUser() async {
-        await _controller.checkUser();
-        if (_controller.getUserRole() != null &&
-                _controller.getUserRole() == RoleEnum.ROLE_ICEMAN) {
-            FirebaseFirestore.instance
-                    .collection(CallRepo.REPO_NAME)
-                    .snapshots()
-                    .listen((event) {
-                calls = event.docs;
-            });
-        }
+  Widget _getFloatingButton(BuildContext context) {
+    if (_controller.isLoggedIn()) {
+      RoleEnum? role = _controller.getUserRole();
+
+      if (role == RoleEnum.ROLE_ICEMAN) {
+        return _floatingSwitchButton(context);
+      } else if (role == RoleEnum.ROLE_USER) {
+        return _floatingCallButton();
+      }
     }
+    return _floatingLoginButton(context);
+  }
 
-    Widget _getFloatingButton(BuildContext context) {
-        if (_controller.isLoggedIn()) {
-            RoleEnum? role = _controller.getUserRole();
+  Widget _floatingCallButton() {
+    return FloatingActionButton(
+      onPressed: () => debugPrint("make call"),
+      child: util.gradientIcon(45, Icons.campaign),
+      backgroundColor: Colors.white,
+    );
+  }
 
-            if (role == RoleEnum.ROLE_ICEMAN) {
-                return _floatingSwitchButton(context);
-            } else if (role == RoleEnum.ROLE_USER) {
-                return _floatingCallButton();
-            }
-        }
-        return _floatingLoginButton(context);
-    }
-
-    Widget _floatingCallButton() {
-        return FloatingActionButton(
-            onPressed: () => debugPrint("make call"),
-            child: util.gradientIcon(45, Icons.campaign),
-            backgroundColor: Colors.white,
+  Widget _floatingSwitchButton(BuildContext context) {
+    return FloatingSwitch(
+      onEnable: () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("GPS ativado"),
+          ),
         );
-    }
-
-    Widget _floatingSwitchButton(BuildContext context) {
-        return FloatingSwitch(
-            onEnable: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text("GPS ativado"),
-                    ),
-                );
-            },
-            onDisable: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text("GPS desativado"),
-                    ),
-                );
-            },
-            icon: Icons.gps_fixed,
-            enabledColor: Colors.lightGreen,
+      },
+      onDisable: () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("GPS desativado"),
+          ),
         );
-    }
+      },
+      icon: Icons.gps_fixed,
+      enabledColor: Colors.lightGreen,
+    );
+  }
 
-    Widget _floatingLoginButton(BuildContext context) {
-        return FloatingActionButton(
-            onPressed: () async {
-                await _controller.showLoginModal(context).then((_) {
-                    setState(() {
-                        debugPrint("update");
-                    });
-                });
-            },
-            child: util.gradientIcon(45, Icons.campaign),
-            backgroundColor: Colors.white,
-        );
-    }
-
-    _getUserLocation() async {
-        await _controller.getClientLocation().then((value) {
-            setState(() {
-                userLocation = value;
-            });
-            mapController.onReady
-                    .then((value) => mapController.move(userLocation!, 17));
+  Widget _floatingLoginButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () async {
+        await _controller.showLoginModal(context).then((_) {
+          setState(() {
+            debugPrint("update");
+          });
         });
-    }
+      },
+      child: util.gradientIcon(45, Icons.campaign),
+      backgroundColor: Colors.white,
+    );
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return StreamBuilder<QuerySnapshot>(
-            stream:
-                    FirebaseFirestore.instance.collection(UserRepo.REPO_NAME).snapshots(),
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            color: primaryColor,
-                        ),
-                    );
-                } else {
-                    debugPrint("Buscou iceman");
-                    icemen = _controller.docsToUserList(snapshot.data!.docs);
-                    return Stack(
-                        children: [
-                            FlutterMap(
-                                mapController: mapController,
-                                options: MapOptions(
-                                    center: userLocation ?? BRAZIL_LAT_LONG,
-                                    zoom: 4,
-                                ),
-                                layers: [
-                                    TileLayerOptions(
-                                        urlTemplate:
-                                                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                                        subdomains: ['a', 'b', 'c'],
-                                        attributionBuilder: (_) {
-                                            return Text(
-                                                "© OpenStreetMap contributors",
-                                                style: TextStyle(
-                                                        color: Colors.grey,
-                                                        fontSize: 12,
-                                                        decoration: TextDecoration.none),
-                                            );
-                                        },
-                                    ),
-                                    MarkerLayerOptions(
-                                        markers: _getMarkers(icemen),
-                                    ),
+  _getUserLocation() async {
+    await _controller.getClientLocation().then((value) {
+      setState(() {
+        userLocation = value;
+      });
+      mapController.onReady
+          .then((value) => mapController.move(userLocation!, 17));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream:
+          FirebaseFirestore.instance.collection(UserRepo.REPO_NAME).snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: primaryColor,
+            ),
+          );
+        } else {
+          debugPrint("Buscou iceman");
+          icemen = _controller.docsToUserList(snapshot.data!.docs);
+          return Stack(
+            children: [
+              FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  center: userLocation ?? BRAZIL_LAT_LONG,
+                  zoom: 4,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c'],
+                    attributionBuilder: (_) {
+                      return Text(
+                        "© OpenStreetMap contributors",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                            decoration: TextDecoration.none),
+                      );
+                    },
+                  ),
+                  MarkerLayerOptions(
+                    markers: _getMarkers(icemen),
+                  ),
+                ],
+              ),
+              Scaffold(
+                drawer: Drawer(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        width: double.infinity,
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: const [
+                            primaryColor,
+                            secondColor,
+                          ],
+                          stops: const [0, 0.55],
+                        )),
+                        child: !_controller.isLoggedIn()
+                            ? null
+                            : Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey,
+                                          shape: BoxShape.circle),
+                                      child: Icon(
+                                        Icons.person,
+                                        color: Colors.white,
+                                        size: 50,
+                                      )),
+                                  Text(
+                                    _controller.user!.name!,
+                                    style: TextStyle(
+                                        fontSize: 28, color: Colors.white),
+                                  ),
+                                  Text(_controller.user!.email!,
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.white)),
                                 ],
-                            ),
-                            Scaffold(
-                                drawer: Drawer(
-                                    child: Center(
-                                            child: ElevatedButton(
-                                        child: Text("Logout"),
-                                        onPressed: () async {
-                                            _controller.logout().then((_) => setState(() {}));
-                                        },
-                                    )),
-                                ),
-                                appBar: AppBar(
-                                    backgroundColor: Colors.transparent,
-                                    foregroundColor: Colors.black,
-                                    shadowColor: Colors.transparent,
-                                ),
-                                backgroundColor: Colors.transparent,
-                                floatingActionButton: _getFloatingButton(context),
-                            ),
-                        ],
-                    );
-                }
-            },
-        );
-    }
-
-    List<Marker> _getMarkers(List<User> icemen) {
-        var positions = <LatLng>[];
-        var markers = <Marker>[];
-
-        //Provisório
-        if (userLocation != null) {
-            markers.add(Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: userLocation!,
-                    builder: (ctx) => Icon(
-                                Icons.place,
-                                color: Colors.red,
-                                size: 50,
-                            )));
-
-            for (var iceman in icemen) {
-                if (iceman.position != null) positions.add(iceman.position!);
-            }
-
-            for (var position in positions) {
-                if ((userLocation!.latitude - position.latitude).abs() <
-                                ICEMEN_LOOK_RANGE &&
-                        (userLocation!.longitude - position.longitude).abs() <
-                                ICEMEN_LOOK_RANGE) {
-                    markers.add(Marker(
-                            width: 80.0,
-                            height: 80.0,
-                            point: position,
-                            builder: (ctx) => AnimatedContainer(
-                                        duration: Duration(milliseconds: 500),
-                                        child: Image(
-                                            image: AssetImage("assets/popsicle.png"),
-                                        ),
-                                    )));
-                }
-            }
+                              ),
+                      ),
+                      !_controller.isLoggedIn() ? Container() : Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: TextButton.icon(
+                            onPressed: () async {
+                              _controller.logout().then((_) {
+                                setState(() {});
+                                Navigator.of(context).pop();
+                              });
+                            },
+                            icon: Icon(Icons.logout),
+                            label: Text(
+                              "Sair",
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.black,
+                  shadowColor: Colors.transparent,
+                ),
+                backgroundColor: Colors.transparent,
+                floatingActionButton: _getFloatingButton(context),
+              ),
+            ],
+          );
         }
+      },
+    );
+  }
 
-        return markers;
+  List<Marker> _getMarkers(List<User> icemen) {
+    var positions = <LatLng>[];
+    var markers = <Marker>[];
+
+    //Provisório
+    if (userLocation != null) {
+      markers.add(Marker(
+          width: 80.0,
+          height: 80.0,
+          point: userLocation!,
+          builder: (ctx) => Icon(
+                Icons.place,
+                color: Colors.red,
+                size: 50,
+              )));
+
+      for (var iceman in icemen) {
+        if (iceman.position != null) positions.add(iceman.position!);
+      }
+
+      for (var position in positions) {
+        if ((userLocation!.latitude - position.latitude).abs() <
+                ICEMEN_LOOK_RANGE &&
+            (userLocation!.longitude - position.longitude).abs() <
+                ICEMEN_LOOK_RANGE) {
+          markers.add(Marker(
+              width: 80.0,
+              height: 80.0,
+              point: position,
+              builder: (ctx) => AnimatedContainer(
+                    duration: Duration(milliseconds: 500),
+                    child: Image(
+                      image: AssetImage("assets/popsicle.png"),
+                    ),
+                  )));
+        }
+      }
     }
+
+    return markers;
+  }
 }
